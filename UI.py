87c2f24e1dce7d86cf1db0705568eeaae1c8b5b1
killergot <disney.132435morozko@mail.ru>
2024-11-11 22:23:33 +0300

    feat: Разделил мой мега класс с логикой и UI на 2 класа, где класс с логикой наследуется от UI класса

--- UI.py
+++ UI.py
@@ -9,10 +9,6 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.uic.properties import int_list
-
-from PacketGen import PacketGenerator
-from decor import except_catch
 
 class Ui_PacketGen(object):
     def setupUi(self, PacketGen):
@@ -241,9 +237,7 @@ class Ui_PacketGen(object):
         self.retranslateUi(PacketGen)
         self.bob.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(PacketGen)
-        self.temp = PacketGenerator()
 
-        self.MyInit()
 
 
     def retranslateUi(self, PacketGen):
@@ -287,98 +281,3 @@ class Ui_PacketGen(object):
         self.label_8.setText(_translate("PacketGen", "L3:"))
         self.label_9.setText(_translate("PacketGen", "L2:"))
         self.just_ip.setText(_translate("PacketGen", "may be just send IP packet?"))
-
-    @except_catch
-    def MyInit(self) -> None:
-        _translate = QtCore.QCoreApplication.translate
-        self.create.clicked.connect(lambda: self.createPacket())
-        for i,v in enumerate(self.temp.getInterfaceList()):
-            self.int_list.addItem("")
-            self.int_list.setItemText(i, _translate("PacketGen", v))
-        self.int_list.currentTextChanged.connect(lambda: self.temp.setInterface(self.int_list.currentText()))
-
-    @except_catch
-    def getFlags(self) -> str:
-        flags: str = ''
-        if self.SYN.isChecked():
-            flags += 'S'
-        if self.RST.isChecked():
-            flags += 'R'
-        if self.URG.isChecked():
-            flags += 'U'
-        if self.ACK.isChecked():
-            flags += 'A'
-        if self.ECN.isChecked():
-            flags += 'E'
-        if self.CWR.isChecked():
-            flags += 'C'
-        if self.PSH.isChecked():
-            flags += 'P'
-        if self.FIN.isChecked():
-            flags += 'F'
-        return flags
-
-
-    @except_catch
-    def createPacket(self) -> None:
-
-        ip_layer= None
-        kwargs: dict[str, str | int] = dict()
-        if self.ip_src_box.isChecked():
-            kwargs['src'] =  self.ip_src_text.toPlainText()
-        if self.ip_dst_box.isChecked():
-            kwargs['dst'] = self.ip_dst_text.toPlainText()
-        kwargs['id_ip'] = int(self.ip_id_text.toPlainText())
-        kwargs['ttl'] = int(self.ip_ttl_text.toPlainText())
-        kwargs = {k: v for k, v in kwargs.items() if len(str(v))}
-        ip_layer =self.temp.getIpPacket(**kwargs)
-
-        if self.just_ip.isChecked():
-           self.temp.sendPacket(ip_layer)
-
-        elif self.icmp.isVisible():
-            kwargs.clear()
-            kwargs['type'] = self.icmp_type_text.toPlainText()
-            if self.icmp_code_box.isChecked():
-                kwargs['code'] = self.icmp_code_text.toPlainText()
-            if self.icmp_id_box.isChecked():
-                kwargs['id'] = self.icmp_id_text.toPlainText()
-            kwargs = {k: int(v) for k, v in kwargs.items() if len(v)}
-
-            icmp_layer = self.temp.getIcmpPacket(**kwargs)
-            self.temp.sendPacket(ip_layer,icmp_layer)
-        elif self.TCP.isVisible():
-            kwargs.clear()
-            kwargs['sport'] = self.tcp_src_port.text()
-            kwargs['dport'] = self.tcp_dst_port.text()
-            kwargs['seq'] = self.tcp_seq.text()
-            kwargs['ack'] = self.tcp_ack.text()
-            kwargs['window'] = self.tcp_window.text()
-            kwargs = {k : int(v) for k,v in kwargs.items() if len(v)}
-
-            kwargs['payload'] = self.tcp_data_text_edit.toPlainText()
-            kwargs['flags'] = self.getFlags()
-
-            tcp_layer =self.temp.getTcpPacket(**kwargs)
-            self.temp.sendPacket(ip_layer,tcp_layer)
-        elif self.UDP.isVisible():
-            kwargs.clear()
-            kwargs['sport'] = self.udp_src_port.text()
-            kwargs['dport'] = self.udp_dst_port.text()
-            kwargs = {k: int(v) for k, v in kwargs.items() if len(v)}
-            kwargs['payload'] = self.udp_data_text.toPlainText()
-
-            udp_layer =self.temp.getUdpPacket(**kwargs)
-            self.temp.sendPacket(ip_layer,udp_layer)
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    PacketGenWin = QtWidgets.QMainWindow()
-    ui = Ui_PacketGen()
-    ui.setupUi(PacketGenWin)
-    PacketGenWin.show()
-    sys.exit(app.exec_())
-
-
